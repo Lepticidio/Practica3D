9bf5dc4da09580aed64cfcd6909f482b068583cc
Lepticidio <miguel.andujar@live.u-tad.com>
2020-07-10 02:00:42 +0200

    Pdf Formula for text projection

--- data/fragment.glsl
+++ data/fragment.glsl
@@ -11,17 +11,19 @@ in vec4 shadow_coord;
 
 void main()
 {
-    float closestDepth = texture2D(shText, shadow_coord.xy).r; 
 
-    float currentDepth = shadow_coord.z;
+    vec4 shadowColor = vec4(1, 1, 1, 1);
+    if ( texture2D(shText, vec2(shadow_coord)).z < shadow_coord.z - 0.0009 )
+    {
+        shadowColor = vec4(fambient, 1);
+    }
+
+
 
 	vec4 textureColor = texture2D( texSampler, ftex);
 
-    vec4 vFragColor =  textureColor  * vec4(fcolor, 1.0f); 
-    if( currentDepth > closestDepth)
-    {
-        vFragColor = vec4(fambient, 1.0f);
-    }
+    vec4 vFragColor = shadowColor* textureColor  * vec4(fcolor, 1.0f); 
+
 
 	gl_FragColor = vFragColor;
     //gl_FragColor = vec4(vec3(currentDepth), 1.0f);
